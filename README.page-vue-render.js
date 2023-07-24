e34647bbac2f0f5ba7603247d7e5f6f612288fea
William Chau Wei Xuan <55905659+WillCWX@users.noreply.github.com>
2023-07-24 13:14:07 +0800

    Dev Site Update. [skip ci]

---
+++ README.page-vue-render.js
@@ -0,0 +1,106 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"sticky":""}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"/index.html","title":"Home"}},[_v("Your Logo")])]},proxy:true},{key:"right",fn:function(){return [_c('li',[_c('form',{staticClass:"navbar-form"},[_c('searchbar',{attrs:{"data":searchData,"placeholder":"Search","on-hit":searchCallback,"menu-align-right":""}})],1)])]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/contents/topic1.html"}},[_v("Topic 1")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/contents/topic2.html"}},[_v("Topic 2")])]),_v(" "),_c('dropdown',{staticClass:"nav-link",scopedSlots:_u([{key:"header",fn:function(){return [_v("Topic 3")]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/contents/topic3a.html"}},[_v("Topic 3a")])]),_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/contents/topic3b.html"}},[_v("Topic 3b")])])])],1)],1),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('overlay-source',{attrs:{"id":"site-nav","tag-name":"nav","to":"site-nav"}},[_c('div',{staticClass:"site-nav-top"},[_c('div',{staticClass:"fw-bold mb-2",staticStyle:{"font-size":"1.25rem"}},[_v("Contents")])]),_v(" "),_c('div',{staticClass:"nav-component slim-scroll"},[_c('site-nav',[_c('overlay-source',{staticClass:"site-nav-list site-nav-list-root",attrs:{"tag-name":"ul","to":"mb-site-nav"}},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/index.html"}},[_v("Home 🏠")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic1.html"}},[_v("Topic 1")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic2.html"}},[_v("Topic 2")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Topic 3 \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic3a.html"}},[_v("Topic 3a")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic3b.html"}},[_v("Topic 3b")])])])])])])],1)],1)]),_v(" "),_c('div',{attrs:{"id":"content-wrapper"}},[_c('breadcrumb'),_v(" "),_m(0),_v(" "),_c('p',[_v("Welcome to GitHub—where millions of developers work together on software. Ready to get started? Let’s learn how this all works by building and publishing your first GitHub Pages website!")]),_v(" "),_m(1),_v(" "),_m(2),_v(" "),_m(3),_v(" "),_m(4),_v(" "),_m(5),_v(" "),_m(6),_v(" "),_m(7),_v(" "),_m(8),_v(" "),_m(9),_v(" "),_m(10),_v(" "),_m(11),_v(" "),_m(12),_v(" "),_m(13),_v(" "),_m(14),_v(" "),_c('img',{attrs:{"width":"1039","alt":"rename_screenshot","src":"https://user-images.githubusercontent.com/18093541/63129466-956cc580-bf85-11e9-92d8-b028dd483fa5.png"}}),_v(" "),_m(15),_v(" "),_c('p',[_v("Congratulations! You just launched your first GitHub Pages website. It's now live to share with the entire world")]),_v(" "),_m(16),_v(" "),_m(17),_v(" "),_m(18),_v(" "),_m(19),_v(" "),_m(20),_v(" "),_c('p',[_v("Let’s start with this template:")]),_v(" "),_m(21),_m(22),_v(" "),_c('img',{attrs:{"width":"997","alt":"edit-this-file","src":"https://user-images.githubusercontent.com/18093541/63131820-0794d880-bf8d-11e9-8b3d-c096355e9389.png"}}),_v(" "),_c('p',[_v("Delete this placeholder line:")]),_v(" "),_m(23),_c('p',[_v("Then, paste the template to line 15 and fill in the blanks.")]),_v(" "),_c('img',{attrs:{"width":"1032","alt":"edit-githuboctocat-index","src":"https://user-images.githubusercontent.com/18093541/63132339-c3a2d300-bf8e-11e9-8222-59c2702f6c42.png"}}),_v(" "),_m(24),_v(" "),_c('img',{attrs:{"width":"1030","alt":"add-my-username","src":"https://user-images.githubusercontent.com/18093541/63131801-efbd5480-bf8c-11e9-9806-89273f027d16.png"}}),_v(" "),_m(25),_v(" "),_c('p',[_v("🎉 You just made your first commit! 🎉")]),_v(" "),_m(26),_v(" "),_m(27),_v(" "),_m(28),_v(" "),_m(29),_v(" "),_m(30),_v(" "),_m(31)],1),_v(" "),_c('overlay-source',{attrs:{"id":"page-nav","tag-name":"nav","to":"page-nav"}},[_c('div',{staticClass:"nav-component slim-scroll"})]),_v(" "),_c('scroll-top-button')],1),_v(" "),_m(32)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{attrs:{"id":"welcome-to-github"}},[_v("Welcome to GitHub"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#welcome-to-github","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"repositories"}},[_v("Repositories"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#repositories","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Right now, we’re in your first GitHub "),_c('strong',[_v("repository")]),_v(". A repository is like a folder or storage space for your project. Your project's repository contains all its files such as code, documentation, images, and more. It also tracks every change that you—or your collaborators—make to each file, so you can always go back to previous versions of your project if you make any mistakes.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("This repository contains three important files: The HTML code for your first website on GitHub, the CSS stylesheet that decorates your website with colors and fonts, and the "),_c('strong',[_v("README")]),_v(" file. It also contains an image folder, with one image file.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"describe-your-project"}},[_v("Describe your project"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#describe-your-project","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("You are currently viewing your project's "),_c('strong',[_v("README")]),_v(" file. "),_c('strong',[_c('em',[_v("README")])]),_v(" files are like cover pages or elevator pitches for your project. They are written in plain text or "),_c('a',{attrs:{"href":"https://guides.github.com/features/mastering-markdown/"}},[_v("Markdown language")]),_v(", and usually include a paragraph describing the project, directions on how to use it, who authored it, and more.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('a',{attrs:{"href":"https://help.github.com/en/articles/about-readmes"}},[_v("Learn more about READMEs")])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"your-first-website"}},[_v("Your first website"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#your-first-website","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('strong',[_v("GitHub Pages")]),_v(" is a free and easy way to create a website using the code that lives in your GitHub repositories. You can use GitHub Pages to build a portfolio of your work, create a personal website, or share a fun project that you coded with the world. GitHub Pages is automatically enabled in this repository, but when you create new repositories in the future, the steps to launch a GitHub Pages website will be slightly different.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('a',{attrs:{"href":"https://pages.github.com/"}},[_v("Learn more about GitHub Pages")])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"rename-this-repository-to-publish-your-site"}},[_v("Rename this repository to publish your site"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#rename-this-repository-to-publish-your-site","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("We've already set-up a GitHub Pages website for you, based on your personal username. This repository is called "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("hello-world")]),_v(", but you'll rename it to: "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("username.github.io")]),_v(", to match your website's URL address. If the first part of the repository doesn’t exactly match your username, it won’t work, so make sure to get it right.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Let's get started! To update this repository’s name, click the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Settings")]),_v(" tab on this page. This will take you to your repository’s settings page.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('a',{attrs:{"href":"https://user-images.githubusercontent.com/18093541/63130482-99e6ad80-bf88-11e9-99a1-d3cf1660b47e.png","target":"_self"}},[_c('img',{staticClass:"img-fluid",attrs:{"src":"https://user-images.githubusercontent.com/18093541/63130482-99e6ad80-bf88-11e9-99a1-d3cf1660b47e.png","alt":"repo-settings-image"}})])])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Under the "),_c('strong',[_v("Repository Name")]),_v(" heading, type: "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("username.github.io")]),_v(", where username is your username on GitHub. Then click "),_c('strong',[_v("Rename")]),_v("—and that’s it. When you’re done, click your repository name or browser’s back button to return to this page.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Once you click "),_c('strong',[_v("Rename")]),_v(", your website will automatically be published at: "),_c('a',{attrs:{"href":"https://your-username.github.io/"}},[_v("https://your-username.github.io/")]),_v(". The HTML file—called "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("index.html")]),_v("—is rendered as the home page and you'll be making changes to this file in the next step.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"making-your-first-edit"}},[_v("Making your first edit"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#making-your-first-edit","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("When you make any change to any file in your project, you’re making a "),_c('strong',[_v("commit")]),_v(". If you fix a typo, update a filename, or edit your code, you can add it to GitHub as a commit. Your commits represent your project’s entire history—and they’re all saved in your project’s repository.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("With each commit, you have the opportunity to write a "),_c('strong',[_v("commit message")]),_v(", a short, meaningful comment describing the change you’re making to a file. So you always know exactly what changed, no matter when you return to a commit.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"practice-customize-your-first-github-website-by-writing-html-code"}},[_v("Practice: Customize your first GitHub website by writing HTML code"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#practice-customize-your-first-github-website-by-writing-html-code","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Want to edit the site you just published? Let’s practice commits by introducing yourself in your "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("index.html")]),_v(" file. Don’t worry about getting it right the first time—you can always build on your introduction later.")])}
+},function anonymous(
+) {
+with(this){return _c('pre',[_c('code',{pre:true,attrs:{"class":"hljs"}},[_c('span',[_v("<p>Hello World! I’m [username]. This is my website!</p>\n")])])])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("To add your introduction, copy our template and click the edit pencil icon at the top right hand corner of the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("index.html")]),_v(" file.")])}
+},function anonymous(
+) {
+with(this){return _c('pre',[_c('code',{pre:true,attrs:{"class":"hljs"}},[_c('span',[_v("<p>Welcome to your first GitHub Pages website!</p>\n")])])])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("When you’re done, scroll down to the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Commit changes")]),_v(" section near the bottom of the edit page. Add a short message explaining your change, like \"Add my introduction\", then click "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Commit changes")]),_v(".")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Once you click "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Commit changes")]),_v(", your changes will automatically be published on your GitHub Pages website. Refresh the page to see your new changes live in action.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"extra-credit-keep-on-building"}},[_v("Extra Credit: Keep on building!"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#extra-credit-keep-on-building","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Change the placeholder Octocat gif on your GitHub Pages website by "),_c('a',{attrs:{"href":"https://myoctocat.com/build-your-octocat/"}},[_v("creating your own personal Octocat emoji")]),_v(" or "),_c('a',{attrs:{"href":"https://octodex.github.com/"}},[_v("choose a different Octocat gif from our logo library here")]),_v(". Add that image to line 12 of your "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("index.html")]),_v(" file, in place of the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("<img src=")]),_v(" link.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Want to add even more code and fun styles to your GitHub Pages website? "),_c('a',{attrs:{"href":"https://github.com/github/personal-website"}},[_v("Follow these instructions")]),_v(" to build a fully-fledged static website.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('a',{attrs:{"href":"/images/create-octocat.png","target":"_self"}},[_c('img',{staticClass:"img-fluid",attrs:{"src":"/images/create-octocat.png","alt":"octocat"}})])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"everything-you-need-to-know-about-github"}},[_v("Everything you need to know about GitHub"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#everything-you-need-to-know-about-github","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Getting started is the hardest part. If there’s anything you’d like to know as you get started with GitHub, try searching "),_c('a',{attrs:{"href":"https://help.github.com"}},[_v("GitHub Help")]),_v(". Our documentation has tutorials on everything from changing your repository settings to configuring GitHub from your command line.")])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Generated by "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.0")]),_v("]")])])])}
+}];
+  
